aba86df8485870ff9a84130933a8b59fde4905b3
Mateus Solar <mateusr.almeidas@gmail.com>
2025-12-14 18:59:00 +0800

    Fixing errors during deserializing json

--- DealershipAPI/Program.cs
+++ DealershipAPI/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSignalR(); 
 
 // Dependency Injection
 builder.Services.AddSingleton<ICarRepository, CarRepository>();
--- DealershipAPI/Repositories/CarRepository.cs
+++ DealershipAPI/Repositories/CarRepository.cs
@@ -18,8 +18,22 @@ namespace DealershipAPI.Repositories
 
         public IReadOnlyList<Car> GetAllCars()
         {
+            if (!File.Exists(_filePath))
+                throw new FileNotFoundException($"File not found: {_filePath}");
             var json = File.ReadAllText(_filePath);
-            return JsonSerializer.Deserialize<List<Car>>(json)!;
+
+            var cars = JsonSerializer.Deserialize<List<Car>>(
+                json,
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                }
+            );
+
+            if (cars == null)
+                throw new Exception("Unable to deserialize the cars.");
+
+            return cars;
         }
     }
 }
